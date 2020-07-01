240add12115a2387782ff4d8af29f62133db8d8f
AndrewIgnacio <andrew.ignacio1@gmail.com>
2020-07-01 11:34:49 -0400

    conditional rendering

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,11 +4,72 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML. JavaScript Syntax Extension. Provided by React.
 // Babel - JavaScript Compiler. babeljs.io
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['one', 'two']
+};
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    app.options.length > 0 ? 'Here are your options' : 'No options',
+    React.createElement(
+        'ol',
+        null,
+        app.options.map(function (option) {
+            return React.createElement(
+                'li',
+                null,
+                option
+            );
+        })
+    )
 );
+
+var user = {
+    name: 'Andrew Ignacio',
+    age: 30,
+    location: 'Annandale'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            user.location
+        );
+    };
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name.toUpperCase() : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,45 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML. JavaScript Syntax Extension. Provided by React.
 // Babel - JavaScript Compiler. babeljs.io
-var template = <h1>Indecision App</h1>;
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: [
+        'one',
+        'two'
+    ]
+}
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options.length > 0 ? 'Here are your options' : 'No options'}
+        <ol>{app.options.map(option => 
+            <li>{option}</li>)
+        }</ol>
+    </div>  
+);
+
+var user = {
+    name: 'Andrew Ignacio',
+    age: 30,
+    location: 'Annandale'
+}
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {user.location}</p>;
+    };
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name.toUpperCase() : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
