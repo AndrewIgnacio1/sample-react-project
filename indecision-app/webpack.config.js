b019a65846c61d79a47a1ba2ffa8dc45e9a6b7a5
AndrewIgnacio <andrew.ignacio1@gmail.com>
2020-07-03 12:06:20 -0400

    removing sripts/app.js and installing and configuring webpack

---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,11 @@
+// on terminal run `node webpack.config.js` to see dirname for file
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
