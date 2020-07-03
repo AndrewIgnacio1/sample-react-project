151caddb2f44bd23d4d1f541de9a6eaed345aed4
AndrewIgnacio <andrew.ignacio1@gmail.com>
2020-07-03 10:20:45 -0400

    adding data localStorage for counter app

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -1,51 +1,60 @@
 class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOne = this.handleAddOne.bind(this);
-        this.handleMinusOne = this.handleMinusOne.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-        this.state = {
-            count: props.count
-        }
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0
     }
-    handleAddOne() {
-        this.setState((prevState) => {
-            return {
-                count: prevState.count + 1
-            }
-        });
+  }
+  componentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    const count = parseInt(stringCount, 10);
+    if (!isNaN(count)) {
+      this.setState(() => ({ count }))
     }
-    handleMinusOne() {
-        this.setState((prevState) => {
-            return {
-                count: prevState.count - 1
-            }
-        });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== 0) {
+      const json = JSON.stringify(this.state.count);
+      localStorage.setItem('count', json);
     }
-    handleReset() {
-        this.setState(() => {
-            return {
-                count: 0
-            }
-        });
-    }
-    render() {
-        return (
-            <div>
-                <h1>Count: {this.state.count}</h1>
-                <button onClick={this.handleAddOne}>+1</button>
-                <button onClick={this.handleMinusOne}>-1</button>
-                <button onClick={this.handleReset}>reset</button>
-            </div>
-        )
-    }
+  }
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    });
+  }
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      }
+    });
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    )
+  }
 }
 
-Counter.defaultProps = {
-    count: 0
-}
-
-ReactDOM.render(<Counter count={5} />, document.getElementById('app'));
+ReactDOM.render(<Counter />, document.getElementById('app'));
 
 // let count = 0;
 // const addOne = () => {
